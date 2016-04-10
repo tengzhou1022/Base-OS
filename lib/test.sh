c3ae302c95a62e3a2c3d92a2e565474055edcb7f
tengzhou <tengzhou1022@gmail.com>
2016-04-10 19:42:40 +0800

    test for home-baseos

--- lib/test.sh
+++ lib/test.sh
@@ -1,3 +1,20 @@
 . ./Echo.sh
 . ./Check.sh
-CheckNotDiffer test1 test2
+
+fileCreate() {
+    local filename
+    filename=${1:-$FILENAME}
+    filename=${filename:-$fileFILENAME}
+    RunCmd "touch '$filename'" 0 "Creating file '$filename'"
+    CheckExists "$filename"
+    EchoResult "$filename found"
+}
+PHASE=${PHASE:-Test}
+echo "PHASE=${PHASE}"
+if [[ "$PHASE" =~ "Test" ]]; then
+fileCreate
+CheckExists "$fileFILENAME"
+else
+  echo fwfewfwe
+fi
+#fileCreate
